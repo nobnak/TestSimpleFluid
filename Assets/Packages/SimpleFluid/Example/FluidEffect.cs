ce6a3d493028adc55c4894875a800fccdc889020
unknown <nnakata@LAB-N991.team-lab.local>
2016-02-08 22:20:47 +0900

    no message

---
+++ Assets/Packages/SimpleFluid/Example/FluidEffect.cs
@@ -0,0 +1,110 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace SimpleFluid {
+
+    public class FluidEffect : MonoBehaviour {
+        public const string PROP_FLUID_TEX = "_FluidTex";
+		public const string PROP_REFERENCE_TEX = "_RefTex";
+        public const string PROP_DT = "_Dt";
+
+		public const string PROP_DIR_AND_CENTER = "_DirAndCenter";
+		public const string PROP_INV_RADIUS = "_InvRadius";
+
+		public Solver solver;
+
+		public Collider viewCollider;
+		public Material forceFieldMat;
+		public float radius = 10f;
+
+        public Material advectMat;
+        public Texture2D imageTex;
+
+		public int lod = 1;
+        public float timeScale = 1f;
+
+        public Material[] outputMats;
+
+    	RenderTexture _imageTex0;
+		RenderTexture _imageTex1;
+
+		Vector3 _mousePos;
+		RenderTexture _forceFieldTex;
+
+    	void Update () {
+			var dt = Time.deltaTime * timeScale;
+			var width = imageTex.width >> lod;
+			var height = imageTex.height >> lod;
+
+			UpdateForceField (width, height);
+			solver.forceTex = _forceFieldTex;
+            solver.Solve(dt, width, height);
+			UpdateImage (dt);
+		}
+        void OnDestroy() {
+            Release();
+        }
+
+		void Release() {
+			ReleaseForceField ();
+			ReleaseImage ();
+        }
+
+		void ReleaseForceField () {
+			Destroy (_forceFieldTex);
+		}
+		void ReleaseImage () {
+			Destroy (_imageTex0);
+			Destroy (_imageTex1);
+		}
+		void UpdateImage (float dt) {
+			InitImage ();
+			solver.SetProperties (advectMat, PROP_FLUID_TEX);
+			advectMat.SetTexture (PROP_REFERENCE_TEX, imageTex);
+    		advectMat.SetFloat (PROP_DT, dt);
+            Graphics.Blit (_imageTex0, _imageTex1, advectMat);
+    		Solver.Swap (ref _imageTex0, ref _imageTex1);
+    	}
+		void InitImage(int width, int height) {
+			if (_imageTex0 == null || _imageTex0.width != width || _imageTex0.height != height) {
+				ReleaseImage();
+				_imageTex0 = new RenderTexture (width, height, 0, RenderTextureFormat.ARGBFloat);
+				_imageTex1 = new RenderTexture (width, height, 0, RenderTextureFormat.ARGBFloat);
+				_imageTex0.filterMode = _imageTex1.filterMode = FilterMode.Bilinear;
+				_imageTex0.wrapMode = _imageTex1.wrapMode = TextureWrapMode.Clamp;
+				var active = RenderTexture.active;
+				RenderTexture.active = _imageTex0;
+				GL.Clear (true, true, Color.clear);
+				RenderTexture.active = active;
+			}
+		}
+		void UpdateForceField(int width, int height) {
+			if (_forceFieldTex == null || _forceFieldTex.width != width || _forceFieldTex.height != height) {
+				ReleaseForceField();
+				_forceFieldTex = new RenderTexture(width, height, 0, RenderTextureFormat.RGFloat);
+			}
+
+			var mousePos = Input.mousePosition;
+			var dx = UpdateMousePos(mousePos);
+			var forceVector = Vector2.zero;
+			var uv = Vector2.zero;
+
+			RaycastHit hit;
+			if (Input.GetMouseButton (0)
+				&& viewCollider.Raycast (Camera.main.ScreenPointToRay (mousePos), out hit, float.MaxValue)) {
+				forceVector = Vector2.ClampMagnitude ((Vector2)dx, 1f);
+				uv = hit.textureCoord;
+			}
+
+			forceFieldMat.SetVector(PROP_DIR_AND_CENTER, 
+				new Vector4(forceVector.x, forceVector.y, uv.x, uv.y));
+			forceFieldMat.SetFloat(PROP_INV_RADIUS, 1f / radius);
+			Graphics.Blit(null, _forceFieldTex, forceFieldMat);
+		}
+		Vector3 UpdateMousePos (Vector3 mousePos) {
+			var dx = mousePos - _mousePos;
+			_mousePos = mousePos;
+			return dx;
+		}
+    }
+}
--- Assets/Packages/SimpleFluid/Example/Viewer.shader
+++ Assets/Packages/SimpleFluid/Example/Viewer.shader
@@ -1,17 +1,16 @@
 ﻿Shader "SimpleFluid/Unlit/Viewer" {
 	Properties {
 		_MainTex ("Texture", 2D) = "white" {}
-		_Scale ("Scale", Float) = 1
-		_DensityMode ("Density Mode", Range(0, 1)) = 0
 	}
 	SubShader {
-		Tags { "RenderType"="Opaque" }
+		Tags { "RenderType"="Transparent" "Queue"="Transparent" }
 		LOD 100
+		//Blend One OneMinusSrcAlpha
+		Blend One One
+		ColorMask RGB
 
 		Pass {
 			CGPROGRAM
-			#define PI 3.14159265359
-			#define RAD2NORM (1.0 / (2.0 * PI))
 			#pragma vertex vert
 			#pragma fragment frag
 			
@@ -29,8 +28,6 @@
 
 			sampler2D _MainTex;
 			float4 _MainTex_ST;
-			float _Scale;
-			float _DensityMode;
 
 			float3 HUE2RGB(float h) {
 				float r = abs(h * 6 - 3) - 1;
@@ -51,11 +48,8 @@
 			}
 			
 			fixed4 frag (v2f i) : SV_Target {
-				float4 u = _Scale * tex2D(_MainTex, i.uv);
-				float h = frac(atan2(u.y, u.x) * RAD2NORM);
-				float4 c = float4(HSV2RGB(float3(h, 1, saturate(length(u.xy)))), 1);
-
-				return lerp(c, u.w, _DensityMode);
+				float4 c = tex2D(_MainTex, i.uv);
+				return c;
 			}
 			ENDCG
 		}
