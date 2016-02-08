6a30d843895176228bfea083655c60c9277fd4ed
Nakata Nobuyuki <nobu.nakata@gmail.com>
2016-02-09 01:45:17 +0900

    no message

--- Assets/Packages/SimpleFluid/Example/FluidEffect.cs
+++ Assets/Packages/SimpleFluid/Example/FluidEffect.cs
@@ -13,17 +13,13 @@ namespace SimpleFluid {
 
 		public Solver solver;
 
-		public Collider viewCollider;
 		public Material forceFieldMat;
 		public float radius = 10f;
 
         public Material advectMat;
-        public Texture2D imageTex;
 
 		public int lod = 1;
         public float timeScale = 1f;
-
-        public Material[] outputMats;
 
     	RenderTexture _imageTex0;
 		RenderTexture _imageTex1;
@@ -31,15 +27,16 @@ namespace SimpleFluid {
 		Vector3 _mousePos;
 		RenderTexture _forceFieldTex;
 
-    	void Update () {
+		void OnRenderImage(RenderTexture src, RenderTexture dst) {
 			var dt = Time.deltaTime * timeScale;
-			var width = imageTex.width >> lod;
-			var height = imageTex.height >> lod;
+			var width = src.width >> lod;
+			var height = src.height >> lod;
 
 			UpdateForceField (width, height);
 			solver.forceTex = _forceFieldTex;
             solver.Solve(dt, width, height);
-			UpdateImage (dt);
+			UpdateImage (dt, src);
+			Graphics.Blit (_imageTex0, dst);
 		}
         void OnDestroy() {
             Release();
@@ -57,25 +54,22 @@ namespace SimpleFluid {
 			Destroy (_imageTex0);
 			Destroy (_imageTex1);
 		}
-		void UpdateImage (float dt) {
-			InitImage ();
+		void UpdateImage (float dt, Texture imageTex) {
+			InitImage (imageTex);
 			solver.SetProperties (advectMat, PROP_FLUID_TEX);
 			advectMat.SetTexture (PROP_REFERENCE_TEX, imageTex);
     		advectMat.SetFloat (PROP_DT, dt);
             Graphics.Blit (_imageTex0, _imageTex1, advectMat);
     		Solver.Swap (ref _imageTex0, ref _imageTex1);
     	}
-		void InitImage(int width, int height) {
-			if (_imageTex0 == null || _imageTex0.width != width || _imageTex0.height != height) {
+		void InitImage(Texture imageTex) {
+			if (_imageTex0 == null || _imageTex0.width != imageTex.width || _imageTex0.height != imageTex.height) {
 				ReleaseImage();
-				_imageTex0 = new RenderTexture (width, height, 0, RenderTextureFormat.ARGBFloat);
-				_imageTex1 = new RenderTexture (width, height, 0, RenderTextureFormat.ARGBFloat);
+				_imageTex0 = new RenderTexture (imageTex.width, imageTex.height, 0, RenderTextureFormat.ARGBFloat);
+				_imageTex1 = new RenderTexture (imageTex.width, imageTex.height, 0, RenderTextureFormat.ARGBFloat);
 				_imageTex0.filterMode = _imageTex1.filterMode = FilterMode.Bilinear;
 				_imageTex0.wrapMode = _imageTex1.wrapMode = TextureWrapMode.Clamp;
-				var active = RenderTexture.active;
-				RenderTexture.active = _imageTex0;
-				GL.Clear (true, true, Color.clear);
-				RenderTexture.active = active;
+				Graphics.Blit (imageTex, _imageTex0);
 			}
 		}
 		void UpdateForceField(int width, int height) {
@@ -89,11 +83,9 @@ namespace SimpleFluid {
 			var forceVector = Vector2.zero;
 			var uv = Vector2.zero;
 
-			RaycastHit hit;
-			if (Input.GetMouseButton (0)
-				&& viewCollider.Raycast (Camera.main.ScreenPointToRay (mousePos), out hit, float.MaxValue)) {
+			if (Input.GetMouseButton (0)) {
+				uv = Camera.main.ScreenToViewportPoint (mousePos);
 				forceVector = Vector2.ClampMagnitude ((Vector2)dx, 1f);
-				uv = hit.textureCoord;
 			}
 
 			forceFieldMat.SetVector(PROP_DIR_AND_CENTER, 
